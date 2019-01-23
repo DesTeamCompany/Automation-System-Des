b0ca8a67a8b22ab78019e4d2a8d58159619a9f66
bormolei <svifer@mail.ru>
2019-01-23 19:02:56 +0300

    My first commit

--- src/main/java/javacode/pages/vk/FriendsPage.java
+++ src/main/java/javacode/pages/vk/FriendsPage.java
@@ -1,10 +1,15 @@
 package javacode.pages.vk;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import ru.sbtqa.tag.pagefactory.annotations.ElementTitle;
 import ru.sbtqa.tag.pagefactory.annotations.PageEntry;
 
 /**
  * Created by Юра on 18.01.2019.
  */
 @PageEntry(title = "Друзья")
-public class FriendsPage extends UserPage {
+public class FriendsPage extends UserPage {@ElementTitle("Друзья онлайн")
+@FindBy(xpath = "//*[@id=\"friends_tab_online\"]/a")
+public WebElement FriendOnline;
 }
--- src/main/java/javacode/stepdefs/CommonStepDefs.java
+++ src/main/java/javacode/stepdefs/CommonStepDefs.java
@@ -3,8 +3,12 @@ package javacode.stepdefs;
 
 import cucumber.api.java.ru.Тогда;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import ru.sbtqa.tag.pagefactory.PageFactory;
 import ru.sbtqa.tag.pagefactory.exceptions.PageException;
+
+import java.util.List;
 
 /**
  * Created by Юра on 09.01.2019.
@@ -16,6 +20,15 @@ public class CommonStepDefs {
         try {
             PageFactory.getInstance().getCurrentPage().clickElementByTitle(name);
         } catch (PageException e) {
+            e.printStackTrace();
+        }
+    }
+    @Тогда ("^список онлайн$")
+        public void Write(){
+        try {
+            List<WebElement> list = PageFactory.getWebDriver().findElements(By.xpath("//div[contains(@class,'friends_field friends_field_title')]"));
+            list.forEach(x-> System.out.println(x.getText()));
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
